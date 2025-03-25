4b6fd7dd07d5da31c5b4ed3d33da67b9fe7f6555
abdelrahman <bad606609@gmail.com>
2025-03-25 14:46:38 +0200

    provide signup cubit

--- lib/feature/auth/presentation/views/sign_up_view.dart
+++ lib/feature/auth/presentation/views/sign_up_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_commerce_app/core/services/get_it_services.dart';
 import 'package:fruit_commerce_app/core/widgets/custom_app_bar.dart';
+import 'package:fruit_commerce_app/feature/auth/domain/repos/auth_repo.dart';
+import 'package:fruit_commerce_app/feature/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import 'package:fruit_commerce_app/feature/auth/presentation/views/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -7,9 +11,12 @@ class SignUpView extends StatelessWidget {
   static const routeName = 'signUp';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: SignUpViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: SignUpViewBody(),
+      ),
     );
   }
 }
